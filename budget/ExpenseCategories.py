12df3af5613e591f3e871b77ac1434d783378c33
martynas-rakickis <martynas.rakickis@telia.lt>
2022-12-16 14:06:50 +0200

    module5

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -12,13 +12,28 @@ def main():
     for a, b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)) :
             print("Sets are NOT equal by subset test")
-    s = '''\
-        from . import Expense
-        expenses = Expense.Expenses()
-        expenses.read_expenses('data/spending_data.csv')'''
-    print(str(s))
-    print(timeit.timeit(stmt = 'expenses.categorize_for_loop()',setup=s,number=100000,globals=globals()))
-    print(timeit.timeit(stmt = 'expenses.categorize_set_comprehension()',setup=s,number=100000,globals=globals()))
+    print(timeit.timeit(stmt = 'expenses.categorize_for_loop()',
+                        setup=
+    '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+    ''',
+                        number=100000,
+                        globals=globals()
+                        )
+                        )
+    print(timeit.timeit(stmt = 'expenses.categorize_set_comprehension()',
+                        setup=
+    '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+    ''',
+                        number=100000,
+                        globals=globals()
+                        )
+                        )
     fig,ax=plt.subplots()
     labels = ['Necessary', 'Food', 'Unnecessary']
     divided_expenses_sum = []
