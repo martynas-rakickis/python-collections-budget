ac91609e0c8fee81a6043d63bd263df1ef9b2bce
martynas-rakickis <martynas.rakickis@telia.lt>
2022-12-16 11:22:47 +0200

    module2

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -29,7 +29,7 @@ def main():
 
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
-    print('The count of all expenses: '+ str(myBudgetList.__len__()))          
+    print('The count of all expenses: '+ str(len(myBudgetList)))
 
 
 if __name__ == '__main__':
